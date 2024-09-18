4c4546ff9ae469bcd73a20058fd9ce66b3ba874c
CastleKen <202380191@psu.palawan.edu.phgit config --global user.name>
2024-09-18 12:28:16 +0800

    done

---
+++ PS03/PS03_02/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS03_02
+{
+    class Program
+    {
+        static void LeapYear(int Year)
+        {
+            if (Year % 4 == 0 && Year % 100 != 0 || Year % 400 == 0)
+            {
+                Console.WriteLine($"The year {Year} is a leap year.");               
+            }
+
+            else
+            {
+                Console.WriteLine($"The year {Year} is not a leap year.");               
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the year: ");
+            int Year = Convert.ToInt32(Console.ReadLine());
+
+            LeapYear(Year);            
+        }
+    }
+}
